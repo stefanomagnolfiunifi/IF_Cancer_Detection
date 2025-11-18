3f861d87e9ef62e15dff641828ae428a4bb64f98
armonia <stefano.magnolfi1@edu.unifi.it>
2025-11-18 16:44:26 +0100

    minor changes

--- main.py
+++ main.py
@@ -60,10 +60,10 @@ def create_and_save_df(bam_folder):
     
 if __name__ == "__main__":
     
-    train_folder = "CHROMOSOME_INSTABILITY_hg38/HEALTHY" 
+    train_folder = "CHROMOSOME_INSTABILITY_hg38/test" 
     
     train_patients_df = csr.process_folder(train_folder)
-    #train_patients_df = pd.read_csv(train_folder + "/dataframe.csv")  
+    #train_patients_df = pd.read_csv(train_folder + "/dataframe.csv", index_col='Patient_ID')  
     
 
     if train_patients_df.empty:
@@ -74,20 +74,21 @@ if __name__ == "__main__":
         print("\n IF training... ")
         
         # Initialize the model
-        iso_forest = IsolationForest(n_estimators=100, contamination=0.1, random_state=42, n_jobs=-1)
+        iso_forest = IsolationForest(n_estimators=1000, contamination=0.33, random_state=42, n_jobs=-1)
         
         # Patient_ID is the index: fit, predict and decision_function exclude it 
         iso_forest.fit(train_patients_df)
         
-        test_folder = "CHROMOSOME_INSTABILITY_hg38"
+        test_folder = "CHROMOSOME_INSTABILITY_hg38/train"
         test_patients_df= csr.process_folder(test_folder)
+        #test_patients_df = pd.read_csv(test_folder + "/dataframe.csv", index_col='Patient_ID')  
 
         predictions = iso_forest.predict(test_patients_df)
         anomaly_scores = iso_forest.decision_function(test_patients_df)
 
         results_df = pd.DataFrame({'Patient_ID': test_patients_df.index, 'Prediction': predictions, 'Anomaly_Score': anomaly_scores}) 
 
-        print("Predictions sum: " + str(sum(predictions == -1)))
+        print("Sum of anomalies found: " + str(sum(predictions == -1)))
 
         print("\n First 5 rows of the result")
         print(results_df.head())
